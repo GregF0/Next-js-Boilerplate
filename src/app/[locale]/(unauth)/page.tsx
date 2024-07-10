02ac1e3e3d9e8027c135600a288f8cc9f487bef0
Greg Follain <34081544+GregFollain@users.noreply.github.com>
2024-07-10 22:55:59 +0200

    up

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -18,30 +18,7 @@ export default function Index() {
   return (
     <>
       <p>
-        Explore our GitHub project for more information about{' '}
-        <a
-          className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-          href="https://github.com/ixartz/Next-js-Boilerplate"
-        >
-          Next.js Boilerplate
-        </a>
-        .
-      </p>
-      <p>
-        Follow{' '}
-        <a
-          className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-          href="https://twitter.com/ixartz"
-          target="_blank"
-        >
-          @Ixartz on Twitter
-        </a>{' '}
-        for updates and more information about the boilerplate.
-      </p>
-      <p>
-        Our sponsors&apos; exceptional support has made this project possible.
-        Their services integrate seamlessly with the boilerplate, and we
-        recommend trying them out.
+        Coming Soon !! 
       </p>
       <h2 className="mt-5 text-2xl font-bold">Sponsors</h2>
       <Sponsors />
